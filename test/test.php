bca139d02bb1342cdc363227e4fe791316e54172
Cyrus <83266402+Ezyrus@users.noreply.github.com>
2024-05-06 15:00:49 +0800

    Update test.php

--- test/test.php
+++ test/test.php
@@ -8,7 +8,7 @@ include '../config/config.php';
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test</title>
+        <title>test</title>
     </head>
 
     <body>
